06dc89a5328f2be1b3476e7c09f2d228ea7f661d
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-10-19 01:11:02 +0700

    Log more loss

--- dead_reckoning_forecast/model/process.py
+++ dead_reckoning_forecast/model/process.py
@@ -13,6 +13,9 @@ def train_epoch(model, loader, opt, loss_fn=nn.MSELoss(reduction="none"), val=Fa
         model.train()
         
     avg_loss = 0
+    avg_prediction_loss = 0
+    avg_internal_prediction_loss = 0
+    avg_reconstruction_loss = 0
     n = 0
 
     for i, batch in enumerate(loader):
@@ -29,47 +32,66 @@ def train_epoch(model, loader, opt, loss_fn=nn.MSELoss(reduction="none"), val=Fa
             opt.zero_grad()
         
         pred, (x_0, x_1), xy_pred = model(x, frames)
-        loss = loss_fn(pred, y)
+        prediction_loss = loss_fn(pred, y)
+        internal_prediction_loss = 0
+        reconstruction_loss = 0
         if not test:
             internal_prediction_loss = loss_fn(x_1, x_0)
             reconstruction_loss = loss_fn(xy_pred, xy)
 
-        #print(loss.shape, w.shape, internal_prediction_loss.shape, reconstruction_loss.shape)
+        #print(prediction_loss.shape, w.shape, internal_prediction_loss.shape, reconstruction_loss.shape)
 
 
-        loss = reduction(loss, dim=-1)
+        prediction_loss = reduction(prediction_loss, dim=-1)
         if not test:
             internal_prediction_loss = reduction(internal_prediction_loss, dim=-1)
             reconstruction_loss = reduction(reconstruction_loss, dim=-1)
 
-        loss = loss * w
-        loss = torch.sum(loss, dim=-1) #this is 1 because it has been normed, this is sum because it's weighted sum
+        prediction_loss = prediction_loss * w
+        prediction_loss = torch.sum(prediction_loss, dim=-1) #this is 1 because it has been normed, this is sum because it's weighted sum
         if not test:
             internal_prediction_loss = torch.mean(internal_prediction_loss, dim=-1) #this is 1 because it has been normed
             reconstruction_loss = torch.mean(reconstruction_loss, dim=-1) #this is 1 because it has been normed
 
+        # these should result in 0 dim tensor
+        prediction_loss = torch.sum(prediction_loss, dim=-1) 
         if not test:
-            loss = loss + internal_prediction_loss + reconstruction_loss
-        loss = torch.sum(loss, dim=-1) # this should result in 0 dim tensor
+            internal_prediction_loss = torch.sum(internal_prediction_loss, dim=-1) 
+            reconstruction_loss = torch.sum(reconstruction_loss, dim=-1) 
+        
+        loss = prediction_loss + internal_prediction_loss + reconstruction_loss
         
         if not val:
             loss.backward()
             opt.step()
         
         loss = loss.item()
-        #print(f"Step loss: {loss}")
+        prediction_loss = prediction_loss.item()
+        if not test:
+            internal_prediction_loss = internal_prediction_loss.item()
+            reconstruction_loss = reconstruction_loss.item()
+        
         avg_loss += loss
+        avg_prediction_loss += prediction_loss
+        avg_internal_prediction_loss += internal_prediction_loss
+        avg_reconstruction_loss += reconstruction_loss
 
         n += b
 
-        print("batch", loss/b)
+        print("batch", loss/b, avg_prediction_loss/b, internal_prediction_loss/b, reconstruction_loss/b)
 
         gc.collect()
         
     avg_loss /= n
+    avg_prediction_loss /= n
+    avg_internal_prediction_loss /= n
+    avg_reconstruction_loss /= n
     
     ret = {
         "avg_loss": avg_loss,
+        "avg_prediction_loss": avg_prediction_loss,
+        "avg_internal_prediction_loss": avg_internal_prediction_loss,
+        "avg_reconstruction_loss": avg_reconstruction_loss,
     }
         
     return ret
