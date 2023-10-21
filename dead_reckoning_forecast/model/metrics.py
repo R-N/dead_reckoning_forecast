0309f62a37a8db87772b9abc678c6e3012b19c3b
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-10-21 21:28:17 +0700

    Update metrics.py

--- dead_reckoning_forecast/model/metrics.py
+++ dead_reckoning_forecast/model/metrics.py
@@ -1,11 +1,11 @@
 import torch
 import torch.nn.functional as F
 
-def get_weights(length, weights=True, dim=-1):
+def get_weights(y, weights=True, dim=-1):
     if weights is None:
-        weights = torch.ones(length)
+        weights = torch.ones(y.size(dim))
     if weights is True:
-        weights = torch.Tensor([1/i for i in range(1, length+1)])
+        weights = torch.Tensor([1/i for i in range(1, y.size(dim)+1)])
     weights = weights / sum(weights)
 
     extra_shape = y.shape[(dim+1):]
@@ -15,7 +15,7 @@ def get_weights(length, weights=True, dim=-1):
     return weights
 
 def mse(pred, y, weights=True, dim=-1):
-    weights = get_weights(y.size(-2), weights=weights, dim=dim)
+    weights = get_weights(y, weights=weights, dim=dim)
     
     se = F.mse_loss(pred, y, reduction="none")
     value = torch.sum(weights * se, dim=dim)
@@ -23,7 +23,7 @@ def mse(pred, y, weights=True, dim=-1):
     return value
 
 def mape(pred, y, weights=True, eps=1e-9, dim=-1):
-    weights = get_weights(y.size(-2), weights=weights, dim=dim)
+    weights = get_weights(y, weights=weights, dim=dim)
 
     ape = torch.abs(y-pred)/torch.clamp(torch.abs(y), min=eps)
     value = torch.sum(weights * ape, dim=dim)
